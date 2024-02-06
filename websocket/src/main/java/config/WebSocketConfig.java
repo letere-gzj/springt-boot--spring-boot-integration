848de919e3576086a7a26bbea3ec1ff681c6855b
gaozijie <1906497996@qq.com>
2024-02-06 15:32:10 +0800

    websocket整合

---
+++ websocket/src/main/java/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author gaozijie
+ * @since 2024-02-06
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        // 自动注册使用@ServerEndpoint注解声明的类
+        return new ServerEndpointExporter();
+    }
+}
---
+++ websocket/src/main/java/websocket/DefaultWebSocket.java
@@ -0,0 +1,59 @@
+package websocket;
+
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author gaozijie
+ * @since 2024-02-06
+ */
+@ServerEndpoint("/default/{userId}")
+@Component
+public class DefaultWebSocket {
+
+    private static final Map<String, Session> SESSION_CACHE = new ConcurrentHashMap<>(16);
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        SESSION_CACHE.put(userId, session);
+        System.out.printf("%s --- 用户:{%s}链接成功，当前在线人数:{%d}\n", LocalDateTime.now(), userId, SESSION_CACHE.size());
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("userId") String userId) {
+        try(Session removeSession = SESSION_CACHE.remove(userId)) {
+            System.out.printf("%s --- 用户:{%s}已下线，当前在线人数:{%d}\n", LocalDateTime.now(), userId, SESSION_CACHE.size());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        System.err.printf("%s --- websocket出现异常:{%s}\n", LocalDateTime.now(), throwable.getMessage());
+    }
+
+    @OnMessage
+    public void onMessage(Session session, @PathParam("userId") String userId, String message) {
+        // websocket接收到消息时调用
+        System.out.printf("%s --- message:{%s}\n", LocalDateTime.now(), message);
+    }
+
+    /**
+     * 向客户端发送消息
+     * @param userId 用户Id
+     * @param message 消息
+     * @throws Exception 异常
+     */
+    public void sendMessage(String userId, String message) throws Exception{
+        Session session = SESSION_CACHE.get(userId);
+        session.getBasicRemote().sendText(message);
+    }
+
+}
